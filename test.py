da5480d3b53c522aa1b86b611d284acacd0f7c77
pnavsingh <pnavsingh@gmail.com>
2026-03-01 00:14:03 -0500

    Basic intit and testing phase

---
+++ test.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY")) #On a hidden .env file if you need it lemme know
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", #Has to be this model bc it doesnt allow 2.0
+    contents="Say hello and tell me you are ready to help identify musical notes!"  #Confirm activation and stuff
+)
+
+print(response.text)
