016e3aee720a31db39fca8abc1c2d5c2bda6d37f
pnavsingh <pnavsingh@gmail.com>
2026-03-01 00:17:39 -0500

    Commentchanges

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from google import genai
 
 load_dotenv()
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY")) #On a hidden .env file if you need it lemme know
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY")) #On a hidden .env file if you need it let me know
 
 response = client.models.generate_content(
     model="gemini-2.5-flash", #Has to be this model bc it doesnt allow 2.0
